0876ddb01ffa5c9509c82e7d21f0a29bfe97308b
sarthak30 <sarthaknsit@gmail.com>
2016-09-25 11:51:29 +0530

    Final Commit

---
+++ config.py
@@ -0,0 +1,38 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    POOL_MAIL_SUBJECT_PREFIX = '[Knowledge POOL]'
+    POOL_MAIL_SENDER = 'Admin <admin@knowledgePOOL.in>'
+    POOL_ADMIN = os.environ.get('POOL_ADMIN')
+    F_POSTS_PER_PAGE = 20
+    POOL_FOLLOWERS_PER_PAGE = 50
+    POOL_COMMENTS_PER_PAGE = 30
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+
+config = {
+    'testing': TestingConfig,
+    'default': DevelopmentConfig
+}
